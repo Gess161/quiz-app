46ea098d5bb3e092cac40dfb0e72b85892c5db76
Hennadii Kotulai <newerr161@gmail.com>
2023-05-17 19:02:46 +0300

    adjusted styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,11 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'main-bg': "url('/bg.jpeg')",
+        'card-bg': "url('/card-bg.jpeg')",
+      },
+      height: {
+        // anything regarding theme can be done here
       },
     },
   },
